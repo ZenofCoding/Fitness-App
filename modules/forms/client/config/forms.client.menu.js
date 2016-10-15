8870c835cb6346e82c8ac47b4975d9df5dc40af0
rowieej <rowena.jaico@gmail.com>
2016-10-15 02:21:55 -0400

    created path to joining challenges and creating challenges.  modified the forms

--- modules/forms/client/config/forms.client.menu.js
+++ modules/forms/client/config/forms.client.menu.js
@@ -17,14 +17,10 @@
             roles: ['*']
         });
 
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Standard',    state: 'app.form-standard'});
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Extended',    state: 'app.form-extended'});
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Validation',  state: 'app.form-validation'});
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Wizard',      state: 'app.form-wizard'});
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Upload',      state: 'app.form-upload'});
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'xEditable',   state: 'app.form-xeditable'});
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Image Crop',  state: 'app.form-imagecrop'});
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'uiSelect',    state: 'app.form-uiselect'});
+      
+        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Join a Challenge',  state: 'app.form-extended'});
+        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Create a Group',  state: 'app.form-validation'});
+        
 
     }
 
