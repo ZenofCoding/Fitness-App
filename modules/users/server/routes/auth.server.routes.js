0c8da138c4bc72f0838d78df17ebf8552a729a49
Angelo Bovino <angelo.bovino@outlook.com>
2016-10-15 00:33:08 -0400

    added user and read:orgs to scope

--- modules/users/server/routes/auth.server.routes.js
+++ modules/users/server/routes/auth.server.routes.js
@@ -48,7 +48,12 @@ module.exports = function (app) {
   app.route('/api/auth/linkedin/callback').get(users.oauthCallback('linkedin'));
 
   // Setting the github oauth routes
-  app.route('/api/auth/github').get(users.oauthCall('github'));
+  app.route('/api/auth/github').get(users.oauthCall('github', {
+    scope: [
+      'user',
+      'read:orgs'
+    ]
+  }));
   app.route('/api/auth/github/callback').get(users.oauthCallback('github'));
 
   // Setting the paypal oauth routes
