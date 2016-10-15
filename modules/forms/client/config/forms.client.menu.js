7401008a395de1a5be72c51b9ac7fdddc016464b
joe shin <joe.shin@live.com>
2016-10-14 22:32:14 -0400

    deleted forms

--- modules/forms/client/config/forms.client.menu.js
+++ modules/forms/client/config/forms.client.menu.js
@@ -17,14 +17,8 @@
             roles: ['*']
         });
 
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Standard',    state: 'app.form-standard'});
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Extended',    state: 'app.form-extended'});
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Validation',  state: 'app.form-validation'});
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Wizard',      state: 'app.form-wizard'});
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Upload',      state: 'app.form-upload'});
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'xEditable',   state: 'app.form-xeditable'});
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Image Crop',  state: 'app.form-imagecrop'});
-        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'uiSelect',    state: 'app.form-uiselect'});
+
+        Menus.addSubMenuItem('sidebar', 'app.form', {title: 'Create A Competition',  state: 'app.form-validation'});  
 
     }
 
