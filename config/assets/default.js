6d32d4ac3f2ab5d841f498ea31b23cde35bdc792
rowieej <rowena.jaico@gmail.com>
2016-10-15 13:20:08 -0400

    add angular back into default.js

--- config/assets/default.js
+++ config/assets/default.js
@@ -4,8 +4,8 @@ module.exports = {
   client: {
     lib: {
       css: [
-        // 'public/lib/bootstrap/dist/css/bootstrap.css',
-        // 'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap.css',
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
       ],
       js: [
         'public/lib/jquery/dist/jquery.js',
@@ -14,19 +14,19 @@ module.exports = {
         'public/lib/angular-animate/angular-animate.js',
         'public/lib/angular-messages/angular-messages.js',
         'public/lib/angular-ui-router/release/angular-ui-router.js',
-        // 'public/lib/angular-ui-utils/ui-utils.js',
+        'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/dist/angular-file-upload.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
 
         ///////
 
-        // 'public/lib/angular/angular.js',
+        'public/lib/angular/angular.js',
         'public/lib/angular-route/angular-route.js',
         'public/lib/angular-cookies/angular-cookies.js',
-        // 'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-animate/angular-animate.js',
         'public/lib/angular-touch/angular-touch.js',
-        // 'public/lib/angular-ui-router/release/angular-ui-router.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/ngstorage/ngStorage.js',
         'public/lib/angular-ui-utils/index.js',
         'public/lib/angular-ui-mask/dist/mask.js',
@@ -37,14 +37,14 @@ module.exports = {
         'public/lib/angular-ui-scroll/dist/ui-scroll.js',
         'public/lib/angular-ui-uploader/dist/uploader.js',
         'public/lib/angular-sanitize/angular-sanitize.js',
-        // 'public/lib/angular-resource/angular-resource.js',
+        'public/lib/angular-resource/angular-resource.js',
         'public/lib/angular-translate/angular-translate.js',
         'public/lib/angular-translate-loader-url/angular-translate-loader-url.js',
         'public/lib/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
         'public/lib/angular-translate-storage-local/angular-translate-storage-local.js',
         'public/lib/angular-translate-storage-cookie/angular-translate-storage-cookie.js',
         'public/lib/oclazyload/dist/ocLazyLoad.js',
-        // 'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-loading-bar/build/loading-bar.js',
         'public/lib/jquery.browser/dist/jquery.browser.js',
         'public/lib/angular-dynamic-locale/dist/tmhDynamicLocale.js',
