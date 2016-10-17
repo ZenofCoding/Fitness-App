6f07fc6f03b8a70e2853662ca81631e9b7f65e25
Angelo Bovino <angelo.bovino@outlook.com>
2016-10-17 18:59:45 -0400

    Changed providerUserProfile data to create more unique, personalized profile

--- modules/users/server/config/strategies/fitbit.js
+++ modules/users/server/config/strategies/fitbit.js
@@ -15,22 +15,23 @@ module.exports = function (config) {
     clientSecret: 'd24dfc6f7bb64f374459e04747b67cf8',
     callbackURL: '/api/auth/fitbit/callback'
   },
-  function (req, accessToken, refreshToken, profile, done) {
+  function (req, accessToken, refreshToken, results, done) {
 
     // Set the provider data and include tokens
-    var providerData = profile._json;
+    var providerData = results._json;
+    var fullName = providerData.user.fullName;
     providerData.accessToken = accessToken;
     providerData.refreshToken = refreshToken;
 
-    console.log(providerData);
+    var splitName = fullName.split(' ');
 
     // Create the user OAuth profile
     var providerUserProfile = {
-      firstName: providerData.user.displayName,
-      lastName: providerData.user.displayName,
-      displayName: providerData.user.displayName,
-      email: "fitbit@fitbit.com",
-      username: "fitbit1",
+      firstName: splitName[0],
+      lastName: splitName[1],
+      displayName: splitName[0] + ' ' + splitName[1],
+      email: results.emails ? results.emails[0].value : undefined,
+      username: splitName[1] + providerData.user.encodedId,
       // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
       profileImageURL: (providerData.user.avatar) ? providerData.user.avatar : undefined,
       // jscs:enable
@@ -38,8 +39,6 @@ module.exports = function (config) {
       providerIdentifierField: 'id',
       providerData: providerData
     };
-
-    console.log("PROVIDER USER PROFILE: " + providerUserProfile);
 
     // Save the user OAuth profile
     users.saveOAuthUserProfile(req, providerUserProfile, done);
